a4563ca43f151478f260177a32e676cbadb48e1a
davi <davieduardocosta.miranda@gmail.com>
2024-06-26 18:38:30 -0300

    lvi

--- lvim/config.lua
+++ lvim/config.lua
@@ -5,6 +5,7 @@ lvim.keys.normal_mode["<leader>q"] = ":BufferKill<CR>"
 lvim.keys.normal_mode["<leader>s"] = ":w<CR>"
 lvim.keys.insert_mode["jk"] = "<ESC>"
 lvim.keys.normal_mode["<TAB>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<S-f>"] = ":Telescope live_grep<CR>"
 
 -- visual
 vim.o.textwidth = 0
@@ -12,5 +13,25 @@ vim.o.number = true
 vim.o.wrapmargin = 0
 vim.o.wrap = true
 vim.o.linebreak = true
-vim.o.clipboard = "unnamedplus" -- download the xclip (if using arch)
+vim.o.clipboard = "unnamedplus"
+vim.o.relativenumber = true
 
+lvim.builtin.alpha.dashboard.section.header.val = {
+
+
+
+".------..------..------..------.",
+"|K.--. ||I.--. ||N.--. ||G.--. |",
+"| :/\\: || (\\/) || :(): || :/\\: |",
+"| :\\/: || :\\/: || ()() || :\\/: |",
+"| '--'K|| '--'I|| '--'N|| '--'G|",
+"`------'`------'`------'`------'",
+}
+
+-- plugins
+lvim.plugins = {
+  { "lunarvim/colorschemes" },
+  { "Mofiqul/dracula.nvim" }
+}
+
+lvim.colorscheme = "dracula"
